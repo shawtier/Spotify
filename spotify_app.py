8de07777f9a823e2de339191208329f5d9c196e4
Jayden Cheung <jayden.cheung@warriorlife.net>
2022-03-09 16:35:13 -0800

    Update spotify_app.py

--- spotify_app.py
+++ spotify_app.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 SPOTIPY_CLIENT_ID="c848c34a824f4a638ea4d5852db0e645"
 SPOTIPY_CLIENT_SECRET="221e2c2c77844e0e871f913c68877275"
-SPOTIPY_REDIRECT_URI="https://share.streamlit.io/shawtier/spotify/main/spotify_app.py"
+SPOTIPY_REDIRECT_URI="share.streamlit.io/shawtier/spotify/main/spotify_app.py"
 SCOPE = "user-top-read"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI, scope=SCOPE))
